9d8d2ad79adb44a48c4e96a23e814d57c100e7e1
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-06-18 01:51:32 +0200

    Added the storage to the App class

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,13 @@
+import { Storage } from '../storage/storage';
+import { LocalStorageStrategy } from '../storage/strategies/local';
+
 export default class App {
+  private storage: Storage;
+
+  constructor() {
+    this.storage = new Storage(new LocalStorageStrategy());
+  }
+
   init(callback = () => {}) {
     if (document.readyState !== 'loading') {
       // Document is already ready, call the callback directly
