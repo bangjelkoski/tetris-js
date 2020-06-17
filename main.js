ee379368c709817e2e53fbea9d9d01bf54875242
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-06-18 01:15:57 +0200

    Facade pattern implemented

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-import { Tetris } from './src/tetris/tetris.js';
-import { drawGrid } from './src/helpers/helpers.js';
+import App from './src/app/app';
+import Tetris from './src/tetris/tetris';
+import { drawGrid } from './src/helpers/helpers';
 
-document.addEventListener('DOMContentLoaded', function (event) {
+new App().init(() => {
   const tetris = new Tetris();
   const grid = tetris.getGrid();
 
@@ -10,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
   tetris.start();
   const interval = setInterval(() => {
     if (tetris.isGameOver()) {
-      alert(
+      console.warn(
         `Game over! You have managed to get total of ${tetris.getPoints()} points`
       );
       clearInterval(interval);
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,1 +1,18 @@
-export default class App {}
+export default class App {
+  init(callback = () => {}) {
+    if (document.readyState !== 'loading') {
+      // Document is already ready, call the callback directly
+      callback();
+    } else if (document.addEventListener) {
+      // All modern browsers to register DOMContentLoaded
+      document.addEventListener('DOMContentLoaded', callback);
+    } else {
+      // Old IE browsers
+      (<any>document).attachEvent('onreadystatechange', function () {
+        if (document.readyState === 'complete') {
+          callback();
+        }
+      });
+    }
+  }
+}
