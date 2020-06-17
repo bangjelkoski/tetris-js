b41bc2b94177e1ef8eebbc4373df77d16f8fe489
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-06-17 23:55:47 +0200

    Refactoring to prototype - tests #1

---
+++ src/tetromino/tetromino.spec.js
@@ -0,0 +1,14 @@
+describe('Patterns implementations for the Tetromino Class', () => {
+  describe('Refactoring to a Prototype', () => {
+    test('tetromino class exists', () => {
+      expect(Tetromino).toBeDefined();
+    });
+
+    test('tetromino class return a instance of Tetromino when invoked', () => {
+      const tetromino = new Tetromino();
+
+      expect(tetromino).toBeDefined();
+      expect(tetromino).toBeInstanceOf(Tetromino);
+    });
+  });
+});
