734d132062dba1a48de5479f971fe2449596ae01
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-06-18 00:44:05 +0200

    App class tests #2

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,3 +1,5 @@
+import App from './app';
+
 /**
  * The idea behind this implementation is to implement a App class
  * that helps us manipulate the dom easily.
@@ -13,6 +15,39 @@ describe('Patterns implementations for the App Class', () => {
 
       expect(app).toBeDefined();
       expect(app).toBeInstanceOf(App);
+    });
+  });
+
+  /**
+   * We can abstract away checking when the DOM is ready
+   * by using the Facade pattern since there are
+   * different checks for different type of browsers
+   */
+  describe('Implementing a method to make onLoad event inter-browser-compatible, making the class implement the Facade pattern', () => {
+    test('the app class should have a init method', () => {
+      const app = new App();
+
+      const hasInitMethod = (object) => {
+        return 'init' in object;
+      };
+
+      expect(hasInitMethod(app)).toBe(true);
+    });
+    test('the app class should have a init method', () => {
+      const app = new App();
+
+      const hasInitMethod = (object) => {
+        return 'init' in object;
+      };
+
+      expect(hasInitMethod(app)).toBe(true);
+    });
+    test('the app init method should call the callback function when the dom is loaded', async () => {
+      const app = new App();
+
+      app.init(() => {
+        expect(true).toBe(true);
+      });
     });
   });
 });
