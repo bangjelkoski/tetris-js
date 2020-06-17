fe7d25996b17ec5292601763ab063042fb114737
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-06-18 01:35:33 +0200

    Storage logic with Strategy pattern tests #2

--- src/storage/storage.spec.ts
+++ src/storage/storage.spec.ts
@@ -24,4 +24,22 @@ describe('Strategy Implementation using TDD', () => {
 
     expect(isStorage(object)).toBe(true);
   });
+
+  test('there should be an Strategy interface', () => {
+    //
+  });
+
+  test('the Strategy interface should include couple of methods we need every Strategy to implement', () => {
+    const isStrategy = (object: any): object is Strategy => {
+      return 'get' in object && 'set' in object && 'remove' in object;
+    };
+
+    const object = {
+      get(key): any {},
+      set(key, value): any {},
+      remove(key): any {},
+    };
+
+    expect(isStrategy(object)).toBe(true);
+  });
 });
