43bbf08c8bec3bb6ba151e5eb9204f8fd3ba46af
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-06-17 23:31:13 +0200

    Refactoring to singleton - tests #1

---
+++ src/tetris/tetris.spec.js
@@ -0,0 +1,14 @@
+describe('Patterns implementations for the Tetris Class', () => {
+  describe('Refactoring to a Singleton', () => {
+    test('tetris class exists', () => {
+      expect(Tetris).toBeDefined();
+    });
+
+    test('tetris class return a instance of Tetris when invoked', () => {
+      const tetris = new Tetris();
+
+      expect(tetris).toBeDefined();
+      expect(tetris).toBeInstanceOf(Tetris);
+    });
+  });
+});
