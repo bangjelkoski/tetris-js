b150487c9eb63324ff067bdb9ea9a234fdf49440
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-06-18 00:02:54 +0200

    Refactoring to prototype - test #2

--- src/tetromino/tetromino.spec.js
+++ src/tetromino/tetromino.spec.js
@@ -12,5 +12,24 @@ describe('Patterns implementations for the Tetromino Class', () => {
       expect(tetromino).toBeDefined();
       expect(tetromino).toBeInstanceOf(Tetromino);
     });
+
+    test('tetromino class should have a clone method', () => {
+      const tetromino = new Tetromino({ row: 1, col: 2 });
+
+      const isPrototype = (object) => {
+        return 'clone' in object;
+      };
+
+      expect(isPrototype(tetromino)).toBe(true);
+    });
+
+    test('the clone method should return an instance of the Tetromino class when its called with same properties as the original object', () => {
+      const tetromino = new Tetromino({ row: 1, col: 2 });
+      const clonedTetromino = tetromino.clone();
+
+      expect(clonedTetromino).toBeDefined();
+      expect(clonedTetromino).toBeInstanceOf(Tetromino);
+      expect(clonedTetromino.getPosition()).toEqual({ row: 1, col: 2 });
+    });
   });
 });
